bd9841daf9091f4551b3f132c942aeba708b53b5
johnkevyn <johnkevyntoguem@yahoo.fr>
2017-12-03 21:12:37 +0100

    Jeu jouable
    Jeu jouable avec les flèches et le rafréchissement automatique du tableau

--- 2048/main.cpp
+++ 2048/main.cpp
@@ -1,11 +1,51 @@
 #include <iostream>
 #include <vector>
 #include"modele.h"
-
+#include<curses.h>
 using namespace std;
 
 int main()
 {
+
+
+    Plateau p;
+	p = plateauInitial();
+
+    int k;
+    initscr();
+    noecho();
+    raw();
+    keypad(stdscr,TRUE);
+
+
+    while(1){
+        mvprintw(0,0, dessine(p).c_str());
+        refresh();
+        k=getch();
+
+        if(k==KEY_LEFT){
+            p=deplacement(p, 7);
+        }
+        else if(k==KEY_RIGHT){
+            p=deplacement(p, 4);
+
+        }
+        else if(k==KEY_DOWN){
+            p=deplacement(p, 2);
+        }
+        else if(k==KEY_UP){
+            p=deplacement(p, 8);
+        }
+    }
+
+    endwin();
+
+
+    //////////////////////////////////////
+    //////////////////////////////////////
+    //////////////////////////////////////
+
+/*
 	string com;
 	bool b;
 	int score;
@@ -44,5 +84,7 @@ int main()
 		cout<<"Bravo! vous avez réussi le défi !!\n";
 	} else {
 		cout<<"Jeu terminé! dommage, vous aurez plus de chance la prochaine fois.\n";
-	}
+	}*/
+	return 0;
+
 }
